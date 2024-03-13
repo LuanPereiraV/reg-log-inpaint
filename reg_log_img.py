34229d24c90880c8387495d15cc1eab91abde863
LuanPereiraV <luancenation@hotmail.com>
2024-03-13 14:26:21 -0300

    Init

--- reg_log_img.py
+++ reg_log_img.py
@@ -1,9 +1,13 @@
 from PIL import Image
 import numpy as np
 
-
 def main():
-    print("oi")
+    base = np.array(Image.open("images/base.png"))
+    mask = np.array(Image.open("images/mask.png"))
+
+    mask = np.all(mask == [0, 255, 0, 255], axis=-1)
+
+    print(mask)
 
 if __name__ == "__main__":
     main()
