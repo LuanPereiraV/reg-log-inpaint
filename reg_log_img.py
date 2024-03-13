2b09f59bdd85835a6b4aa21eecefd05cb297647b
LuanPereiraV <luancenation@hotmail.com>
2024-03-13 14:57:08 -0300

    Apply mask

--- reg_log_img.py
+++ reg_log_img.py
@@ -1,13 +1,38 @@
 from PIL import Image
 import numpy as np
+import matplotlib.pyplot as plt
+
+def get_mask(img, mask_color=[0, 255, 0, 255]):
+    return np.all(img == mask_color, axis=-1)
 
 def main():
     base = np.array(Image.open("images/base.png"))
     mask = np.array(Image.open("images/mask.png"))
 
-    mask = np.all(mask == [0, 255, 0, 255], axis=-1)
+    mask = get_mask(mask)
 
-    print(mask)
+    r_base = base[:, :, 0]
+    g_base = base[:, :, 1]
+    b_base = base[:, :, 2]
+
+    fig, axs = plt.subplots(3, 3)
+    axs[0, 0].imshow(r_base, cmap='hot', interpolation='nearest')
+    axs[0, 0].set_title('Red')
+    axs[0, 1].imshow(g_base, cmap='hot', interpolation='nearest')
+    axs[0, 1].set_title('Green')
+    axs[0, 2].imshow(b_base, cmap='hot', interpolation='nearest')
+    axs[0, 2].set_title('Blue')
+
+    axs[1, 0].imshow(mask, cmap='hot', interpolation='nearest')
+    axs[1, 1].imshow(mask, cmap='hot', interpolation='nearest')
+    axs[1, 2].imshow(mask, cmap='hot', interpolation='nearest')
+
+    axs[2, 0].imshow(np.ma.masked_where(mask, r_base), cmap='hot', interpolation='nearest')
+    axs[2, 1].imshow(np.ma.masked_where(mask, g_base), cmap='hot', interpolation='nearest')
+    axs[2, 2].imshow(np.ma.masked_where(mask, b_base), cmap='hot', interpolation='nearest')
+
+    # plt.imshow(b_base, cmap='hot', interpolation='nearest')
+    plt.show()
 
 if __name__ == "__main__":
     main()
